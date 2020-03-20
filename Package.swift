569c35f9755d6bb35a966e152779b0a56c58d78b
Ricardo <6081828+Daedren@users.noreply.github.com>
2020-03-20 16:48:58 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .target(
             name: "LKAlertController",
-            sources: "Pod/Classes/**/*"
+            sources: "Pod/Classes/**/*",
             path: "."
         )
     ]
