7efc435ae39cce08559a8264fa100f00d3d17262
Ricardo <6081828+Daedren@users.noreply.github.com>
2020-03-20 16:29:03 +0000

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "LibraryName",
+    // platforms: [.iOS("8.0"), .tvOS("9.0")],
+    products: [
+        .library(name: "LKAlertController", targets: ["LKAlertController"])
+    ],
+    targets: [
+        .target(
+            name: "LKAlertController",
+            sources: "Pod/Classes/**/*"
+            path: "."
+        )
+    ]
+)
